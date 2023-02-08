7a677d1dc4fb89baacebe6f59acfdb8418c39e7b
Martijn Verwoert <118814404+Martijn28405@users.noreply.github.com>
2023-02-08 16:43:33 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Give some text");
+string text = Console.ReadLine();
+
+Console.WriteLine("What do you want to do with this text?");
+Console.WriteLine("U: make all uppercase");
+Console.WriteLine("L: make all lowercase");
+Console.WriteLine("Any other key: do not change");
+string choice = Console.ReadLine();
+
+if (choice == "U")
+{
+    string newText = text.ToUpper();
+}
+else if (choice == "L")
+{
+    string newText = text.ToLower();
+}
+else
+{
+    string newText = text;
+}
+
+Console.WriteLine(newText);
