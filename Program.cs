cffec2a9af092154392006e5f7b1b25873c26559
Martijn28405 <1049334@hr.nl>
2023-02-09 16:25:06 +0100

    add

--- Program.cs
+++ Program.cs
@@ -1,23 +1,16 @@
-﻿Console.WriteLine("Give some text");
-string text = Console.ReadLine();
-
-Console.WriteLine("What do you want to do with this text?");
-Console.WriteLine("U: make all uppercase");
-Console.WriteLine("L: make all lowercase");
-Console.WriteLine("Any other key: do not change");
-string choice = Console.ReadLine();
-
-if (choice == "U")
+﻿Console.WriteLine("What is your age?");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("What is the age of the student next to you?");
+int studentage = Convert.ToInt32(Console.ReadLine());
+if (age == studentage)
 {
-    string newText = text.ToUpper();
-}
-else if (choice == "L")
+    Console.WriteLine("Your ages are equal");
+} 
+else if (age < studentage)
 {
-    string newText = text.ToLower();
-}
-else
+    Console.WriteLine("You are younger");
+}    
+else if (age > studentage)
 {
-    string newText = text;
-}
-
-Console.WriteLine(newText);
+    Console.WriteLine("You are older");
+}    
