646e6b92878f1db2377e2795c8b47f09c702a2a1
mjvdnet <mjvdnet@gmail.com>
2023-03-22 14:18:09 +0100

    spelling fix

--- Presentation/UserLogin.cs
+++ Presentation/UserLogin.cs
@@ -6,7 +6,7 @@ static class UserLogin
     public static void Start()
     {
         Console.WriteLine("Welcome to the login page");
-        Console.WriteLine($"Please enter your email address:");
+        Console.WriteLine("Please enter your email address:");
         string email = Console.ReadLine();
         Console.WriteLine("Please enter your password:");
         string password = Console.ReadLine();
