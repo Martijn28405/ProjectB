83c09082d76f0e7c38d134cc09ccbe2158f84df4
Jullian0908 <j.goncalves0908@gmail.com>
2023-03-28 14:44:38 +0200

    Added BuySnack method

--- Logic/SnacksLogic.cs
+++ Logic/SnacksLogic.cs
@@ -6,6 +6,7 @@ public class SnacksLogic
 {
     private JsonAccessor<SnackModel> _accesor;
     public List<SnackModel> snacks;
+    public List<SnackModel>? shoppingBag;
     public SnacksLogic()
     {
         _accesor = new JsonAccessor<SnackModel>(@"DataSources/snacks.json");
@@ -36,16 +37,80 @@ public class SnacksLogic
     }
     public void BuySnacks()
     {
-        // Console.WriteLine("Which snacks would you like?");
-        // string choiceSnack = Console.ReadLine();
-        // if (choiceSnack != null)
-        // {
-        //     food.Add(choiceSnack);
-        //     foreach (SnackModel product in food)
-        //     {
-        //         Console.WriteLine($"IN YOUR SHOPPINGBAG: {product}");
-        //     }
-        // }
+        shoppingBag = new List<SnackModel>();
+        while (true)
+        {
+            Console.WriteLine("Do you want to buy a snack?");
+            Console.WriteLine("Y/N");
+            string start = Console.ReadLine().ToUpper();
+            if (start == "Y")
+            {
+                Console.WriteLine("What snack do you want to buy?");
+                foreach (var item in snacks)
+                {
+                    Console.WriteLine($"SNACK: {item.NameFood}");
+                    Console.WriteLine($"PRICE: {item.PriceFood}");
+                }
+                string snackName = Console.ReadLine();
+                foreach (var item in snacks)
+                {
+                    if (item.NameFood == snackName)
+                    {
+                        Console.WriteLine($"SNACK: {item.NameFood}");
+                        Console.WriteLine($"PRICE: {item.PriceFood}");
+                    }
+                }
+                Console.WriteLine("Do you want to buy this snack?");
+                Console.WriteLine("Y/N");
+                string answer = Console.ReadLine().ToUpper();
+                if (answer == "Y")
+                {
+                    Console.WriteLine("How many do you want to buy?");
+                    int amount = Convert.ToInt32(Console.ReadLine());
+                    foreach (var item in snacks)
+                    {
+                        if (item.NameFood == snackName)
+                        {
+                            double totalPrice = item.PriceFood * amount;
+                            Console.WriteLine($"The total price is: {totalPrice}");
+                            Console.WriteLine("Are you sure?");
+                            Console.WriteLine("Y/N");
+                            string answer2 = Console.ReadLine().ToUpper();
+                            if (answer2 == "Y")
+                            {
+                                shoppingBag.Add(new SnackModel(snackName, item.PriceFood));
+                                Console.WriteLine($"Added {amount}x {snackName} to cart!");
+                                Console.WriteLine("Your current shopping bag:");
+                                foreach (var item2 in shoppingBag)
+                                {
+                                    Console.WriteLine($"SNACK: {amount}x {item2.NameFood}");
+                                    Console.WriteLine($"PRICE: {amount * item2.PriceFood}");
+                                }
+
+                            }
+                            else
+                            {
+                                Console.WriteLine("Press any key to return to main menu");
+                                Console.ReadKey(true);
+                                AccountMenu.Start();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Press any key to return to main menu");
+                    Console.ReadKey(true);
+                    AccountMenu.Start();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Press any key to return to main menu");
+                Console.ReadKey(true);
+                AccountMenu.Start();
+            }
+        }
     }
 
 }
--- Presentation/AccountMenu.cs
+++ Presentation/AccountMenu.cs
@@ -15,7 +15,7 @@
                                                                                                                                 
                                                                                                                                 
 ";
-        string[] options = { "View Movies", "View Catering",  "Back to Main Menu", "Log Out", "Exit App" };
+        string[] options = { "View Movies", "View Catering", "Buy Snacks", "Back to Main Menu", "Log Out", "Exit App" };
         Menu accountmenu = new Menu(prompt, options);
         int SelectedIndex = accountmenu.Run();
         switch (SelectedIndex)
@@ -29,10 +29,14 @@
                 viewsnacks.ShowSnacks();
                 break;
             case 2:
+                SnacksLogic buysnacks = new SnacksLogic();
+                buysnacks.BuySnacks();
+                break;
+            case 3:
                 Thread.Sleep(1000);
                 Program.Main();
                 break;
-            case 3:
+            case 4:
                 Console.WriteLine("Press any key to confirm");
                 Console.ReadKey(true);
                 Program.FullName = default;
@@ -40,11 +44,12 @@
                 Program.Password = default;
                 Program.Main();
                 break;
-            case 4:
+            case 5:
                 Console.WriteLine("press any key to exit the app");
                 Console.ReadKey(true);
                 Environment.Exit(0);
                 break;
+
         }
 
     }
