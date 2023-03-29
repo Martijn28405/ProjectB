0d3ac85687c660f1283b175fcf4d9f5097cbe0a3
Martijn Verwoert <118814404+Martijn28405@users.noreply.github.com>
2023-03-29 11:48:47 +0200

    All Functions that aren't working yet dont break right away

---
+++ AccountMenu.cs
@@ -0,0 +1,57 @@
+﻿public class AccountMenu
+{
+    public static void Start()
+    {
+        Console.CursorVisible = false;
+
+        string prompt = @"
+ ________  ________  ________  ________  ___  ___  ________   _________        _____ ______   _______   ________   ___  ___     
+|\   __  \|\   ____\|\   ____\|\   __  \|\  \|\  \|\   ___  \|\___   ___\     |\   _ \  _   \|\  ___ \ |\   ___  \|\  \|\  \    
+\ \  \|\  \ \  \___|\ \  \___|\ \  \|\  \ \  \\\  \ \  \\ \  \|___ \  \_|     \ \  \\\__\ \  \ \   __/|\ \  \\ \  \ \  \\\  \   
+ \ \   __  \ \  \    \ \  \    \ \  \\\  \ \  \\\  \ \  \\ \  \   \ \  \       \ \  \\|__| \  \ \  \_|/_\ \  \\ \  \ \  \\\  \  
+  \ \  \ \  \ \  \____\ \  \____\ \  \\\  \ \  \\\  \ \  \\ \  \   \ \  \       \ \  \    \ \  \ \  \_|\ \ \  \\ \  \ \  \\\  \ 
+   \ \__\ \__\ \_______\ \_______\ \_______\ \_______\ \__\\ \__\   \ \__\       \ \__\    \ \__\ \_______\ \__\\ \__\ \_______\
+    \|__|\|__|\|_______|\|_______|\|_______|\|_______|\|__| \|__|    \|__|        \|__|     \|__|\|_______|\|__| \|__|\|_______|
+                                                                                                                                
+                                                                                                                                
+                                                                                                                                
+";
+        string[] options = { "View Movies", "View Catering", "Buy Snacks", "Back to Main Menu", "Log Out", "Exit App" };
+        Menu accountmenu = new Menu(prompt, options);
+        int SelectedIndex = accountmenu.Run();
+        switch (SelectedIndex)
+        {
+            case 0:
+                MoviesLogic showmovies = new MoviesLogic();
+                showmovies.ShowMovies();
+                break;
+            case 1:
+                SnacksLogic viewsnacks = new SnacksLogic();
+                viewsnacks.ShowSnacks();
+                break;
+            case 2:
+                SnacksLogic buysnacks = new SnacksLogic();
+                buysnacks.BuySnacks();
+                break;
+            case 3:
+                Thread.Sleep(1000);
+                Program.Main();
+                break;
+            case 4:
+                Console.WriteLine("Press any key to confirm");
+                Console.ReadKey(true);
+                Program.FullName = default;
+                Program.Email = default;
+                Program.Password = default; ;
+                Program.Main();
+                break;
+            case 5:
+                Console.WriteLine("press any key to exit the app");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+                break;
+
+        }
+
+    }
+}
---
+++ UserLogin.cs
@@ -0,0 +1,71 @@
+using System.ComponentModel.Design;
+
+public class UserLogin
+{
+    static private AccountsLogic accountsLogic = new AccountsLogic();
+
+
+    public static void Start()
+    {
+        Console.CursorVisible = false;
+        string prompt = @"
+
+ ___  ___  ________  _______   ________          ___       ________  ________  ___  ________           ________  ________  ________  _______      
+|\  \|\  \|\   ____\|\  ___ \ |\   __  \        |\  \     |\   __  \|\   ____\|\  \|\   ___  \        |\   __  \|\   __  \|\   ____\|\  ___ \     
+\ \  \\\  \ \  \___|\ \   __/|\ \  \|\  \       \ \  \    \ \  \|\  \ \  \___|\ \  \ \  \\ \  \       \ \  \|\  \ \  \|\  \ \  \___|\ \   __/|    
+ \ \  \\\  \ \_____  \ \  \_|/_\ \   _  _\       \ \  \    \ \  \\\  \ \  \  __\ \  \ \  \\ \  \       \ \   ____\ \   __  \ \  \  __\ \  \_|/__  
+  \ \  \\\  \|____|\  \ \  \_|\ \ \  \\  \|       \ \  \____\ \  \\\  \ \  \|\  \ \  \ \  \\ \  \       \ \  \___|\ \  \ \  \ \  \|\  \ \  \_|\ \ 
+   \ \_______\____\_\  \ \_______\ \__\\ _\        \ \_______\ \_______\ \_______\ \__\ \__\\ \__\       \ \__\    \ \__\ \__\ \_______\ \_______\
+    \|_______|\_________\|_______|\|__|\|__|        \|_______|\|_______|\|_______|\|__|\|__| \|__|        \|__|     \|__|\|__|\|_______|\|_______|
+             \|_________|                                                                                                                         
+                                                                                                                                                  
+                                                                                                                                                  
+   
+                                                                                                            
+                                                                                                            
+                                                                                                            
+";
+        string[] options = { "Login" };
+        Menu loginmenu = new Menu(prompt, options);
+        int SelectedIndex = loginmenu.Run();
+        switch (SelectedIndex)
+        {
+            case 0:
+                if (Program.Email == null)
+                {
+                    Console.WriteLine("Welcome to the login page");
+                    Console.WriteLine("Please enter your email address:");
+                    string email = Console.ReadLine();
+                    Console.WriteLine("Please enter your password:");
+                    string password = Console.ReadLine();
+                    AccountModel acc = accountsLogic.CheckLogin(email, password);
+                    if (acc != null)
+                    {
+                        Console.WriteLine("Welcome back " + acc.FullName);
+                        Console.WriteLine("Your e-mail is " + acc.EmailAddress);
+                        Program.FullName = acc.FullName;
+                        Program.Email = acc.EmailAddress;
+                        Program.Password = acc.Password;
+                        Console.WriteLine("Press any key to continue to the Menu");
+                        Console.ReadKey(true);
+                        AccountMenu.Start();
+                    }
+                    else
+                    {
+                        Console.WriteLine("No account found with that email and password");
+                        Console.WriteLine("Press any key to return to the login screen");
+                        Console.ReadKey(true);
+
+                    }
+
+                }
+
+                else if (Program.Email != null)
+            {
+                AccountMenu.Start();
+            }
+
+                break;
+        }      
+        }
+    }
