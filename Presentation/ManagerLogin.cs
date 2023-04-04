be06e9b4099e5feedacc0c2e232c326e17ede399
Martijn28405 <1049334@hr.nl>
2023-04-04 15:39:45 +0200

    Manager kan nu inloggen en een account aanmaken

---
+++ Presentation/ManagerLogin.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel.Design;
+
+public class ManagerLogin
+{
+    static private ManagerAccountsLogic accountsLogic = new ManagerAccountsLogic();
+
+
+    public static void Start()
+    {
+        Console.CursorVisible = false;
+        string prompt = @"
+
+
+ _____ ______   ________  ________   ________  ________  _______   ________          ___       ________  ________  ___  ________      
+|\   _ \  _   \|\   __  \|\   ___  \|\   __  \|\   ____\|\  ___ \ |\   __  \        |\  \     |\   __  \|\   ____\|\  \|\   ___  \    
+\ \  \\\__\ \  \ \  \|\  \ \  \\ \  \ \  \|\  \ \  \___|\ \   __/|\ \  \|\  \       \ \  \    \ \  \|\  \ \  \___|\ \  \ \  \\ \  \   
+ \ \  \\|__| \  \ \   __  \ \  \\ \  \ \   __  \ \  \  __\ \  \_|/_\ \   _  _\       \ \  \    \ \  \\\  \ \  \  __\ \  \ \  \\ \  \  
+  \ \  \    \ \  \ \  \ \  \ \  \\ \  \ \  \ \  \ \  \|\  \ \  \_|\ \ \  \\  \|       \ \  \____\ \  \\\  \ \  \|\  \ \  \ \  \\ \  \ 
+   \ \__\    \ \__\ \__\ \__\ \__\\ \__\ \__\ \__\ \_______\ \_______\ \__\\ _\        \ \_______\ \_______\ \_______\ \__\ \__\\ \__\
+    \|__|     \|__|\|__|\|__|\|__| \|__|\|__|\|__|\|_______|\|_______|\|__|\|__|        \|_______|\|_______|\|_______|\|__|\|__| \|___                                                                                                           
+";
+        string[] options = { "Login", "Add Account", "Back to Main Menu" };
+        Menu loginmenu = new Menu(prompt, options);
+        int SelectedIndex = loginmenu.Run();
+        switch (SelectedIndex)
+        {
+            case 0:
+                if (Program.Email == null)
+                {
+                    Console.WriteLine("Welcome to the login page");
+                    Console.WriteLine("Please enter your email address:");
+                    string email = Console.ReadLine();
+                    Console.WriteLine("Please enter your password:");
+                    string password = Console.ReadLine();
+                    ManagerAccountModel acc = accountsLogic.CheckLogin(email, password);
+                    if (acc != null)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Login Successful");
+                        Console.ResetColor();
+                        Console.WriteLine("Welcome back " + acc.FullName);
+                        Console.WriteLine("Your e-mail is " + acc.EmailAddress);
+                        Program.Manager_FullName = acc.FullName;
+                        Program.Manager_Email = acc.EmailAddress;
+                        Program.Manager_Password = acc.Password;
+                        Console.WriteLine("Press any key to continue to the Menu");
+                        Console.ReadKey(true);
+                        ManagerMenu.Start();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Login Failed");
+                        Console.ResetColor();
+                        Console.WriteLine("No account found with that email and password");
+                        Console.WriteLine("Press any key to return to the login screen");
+                        Console.ReadKey(true);
+                        Start();
+
+                    }
+
+                }
+
+                else if (Program.Email != null)
+                {
+                    AccountMenu.Start();
+                }
+
+                break;
+            case 1:
+                ManagerAccountsLogic add_account = new ManagerAccountsLogic();
+                add_account.CreateAccount();
+                break;
+            case 2:
+                Program.Main();
+                break;
+        }
+    }
+}
---
+++ Presentation/SeatMenu.cs
@@ -0,0 +1,128 @@
+﻿class SeatMenu
+    {
+        private int selectedSeatIndex = 0; // Initially select the first seat
+        private string[,] seats = new string[5, 5]; // 5x5 array of seats
+        
+        public void Show_Seats()
+        {
+            InitializeSeats(); // Initialize seats with "[ ]"
+            DrawSeats(); // Draw initial seats
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        MoveSelectionUp();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        MoveSelectionDown();
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        MoveSelectionLeft();
+                        break;
+                    case ConsoleKey.RightArrow:
+                        MoveSelectionRight();
+                        break;
+                    case ConsoleKey.Enter:
+                        SelectSeat();
+                        break;
+                }
+            }
+        }
+
+        private void InitializeSeats()
+        {
+            for (int i = 0; i < seats.GetLength(0); i++)
+            {
+                for (int j = 0; j < seats.GetLength(1); j++)
+                {
+                    seats[i, j] = "[ ]";
+                }
+            }
+        }
+
+        public void DrawSeats()
+        {
+            Console.Clear();
+            Console.WriteLine("Select a seat:");
+            for (int i = 0; i < seats.GetLength(0); i++)
+            {
+                for (int j = 0; j < seats.GetLength(1); j++)
+                {
+                    if (i == selectedSeatIndex / seats.GetLength(1) && j == selectedSeatIndex % seats.GetLength(1))
+                    {
+                        Console.BackgroundColor = ConsoleColor.Green; // Highlight selected seat
+                    }
+                    Console.Write(seats[i, j] + " ");
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private void MoveSelectionUp()
+        {
+            if (selectedSeatIndex >= seats.GetLength(1))
+            {
+                selectedSeatIndex -= seats.GetLength(1);
+                DrawSeats();
+            }
+        }
+
+        private void MoveSelectionDown()
+        {
+            if (selectedSeatIndex < seats.GetLength(0) * seats.GetLength(1) - seats.GetLength(1))
+            {
+                selectedSeatIndex += seats.GetLength(1);
+                DrawSeats();
+            }
+        }
+
+        private void MoveSelectionLeft()
+        {
+            if (selectedSeatIndex % seats.GetLength(1) > 0)
+            {
+                selectedSeatIndex--;
+                DrawSeats();
+            }
+        }
+
+        private void MoveSelectionRight()
+        {
+            if (selectedSeatIndex % seats.GetLength(1) < seats.GetLength(1) - 1)
+            {
+                selectedSeatIndex++;
+                DrawSeats();
+            }
+        }
+
+        private void SelectSeat()
+        {
+            int row = selectedSeatIndex / seats.GetLength(1);
+            int col = selectedSeatIndex % seats.GetLength(1);
+            if (seats[row, col] == "[ ]")
+            {
+                seats[row, col] = "[X]"; // Mark the seat as taken
+                DrawSeats();
+                Console.WriteLine("Seat selected!");
+                Console.WriteLine("Want to select more? ");
+                string choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "y":
+                        DrawSeats();
+                        break;
+                    case "n":
+                        Program.Main();
+                        break;
+                }
+                DrawSeats();
+            }
+            else
+            {
+                Console.WriteLine("Seat already taken. Please select another seat.");
+                
+            }
+        }
+    }
