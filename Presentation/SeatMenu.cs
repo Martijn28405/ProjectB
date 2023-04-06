7cf2545c3c51dd8851efed34d4834566f0a4eb66
Revin400 <57983479+Revin400@users.noreply.github.com>
2023-04-06 15:02:11 +0200

    Add files via upload
    The small Cinema Room is created with the corosponding exeptions and the base color blue is added

--- Presentation/SeatMenu.cs
+++ Presentation/SeatMenu.cs
@@ -1,128 +1,181 @@
-﻿class SeatMenu
+class Seatmenu
+{
+    // Small cinema
+    static int selectedSeatIndex = 6; // Initially select the first seat
+    static string[,] seats = new string[14, 12]; // 14x12 array of seats
+
+    static void Main(string[] args)
     {
-        private int selectedSeatIndex = 0; // Initially select the first seat
-        private string[,] seats = new string[5, 5]; // 5x5 array of seats
-        
-        public void Show_Seats()
+        InitializeSeats(); // Initialize seats with "[ ]"
+        DrawSeats(); // Draw initial seats
+        while (true)
         {
-            InitializeSeats(); // Initialize seats with "[ ]"
-            DrawSeats(); // Draw initial seats
-            while (true)
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
             {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-                switch (keyInfo.Key)
-                {
-                    case ConsoleKey.UpArrow:
-                        MoveSelectionUp();
-                        break;
-                    case ConsoleKey.DownArrow:
-                        MoveSelectionDown();
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        MoveSelectionLeft();
-                        break;
-                    case ConsoleKey.RightArrow:
-                        MoveSelectionRight();
-                        break;
-                    case ConsoleKey.Enter:
-                        SelectSeat();
-                        break;
-                }
-            }
-        }
-
-        private void InitializeSeats()
-        {
-            for (int i = 0; i < seats.GetLength(0); i++)
-            {
-                for (int j = 0; j < seats.GetLength(1); j++)
-                {
-                    seats[i, j] = "[ ]";
-                }
-            }
-        }
-
-        public void DrawSeats()
-        {
-            Console.Clear();
-            Console.WriteLine("Select a seat:");
-            for (int i = 0; i < seats.GetLength(0); i++)
-            {
-                for (int j = 0; j < seats.GetLength(1); j++)
-                {
-                    if (i == selectedSeatIndex / seats.GetLength(1) && j == selectedSeatIndex % seats.GetLength(1))
-                    {
-                        Console.BackgroundColor = ConsoleColor.Green; // Highlight selected seat
-                    }
-                    Console.Write(seats[i, j] + " ");
-                    Console.ResetColor();
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private void MoveSelectionUp()
-        {
-            if (selectedSeatIndex >= seats.GetLength(1))
-            {
-                selectedSeatIndex -= seats.GetLength(1);
-                DrawSeats();
-            }
-        }
-
-        private void MoveSelectionDown()
-        {
-            if (selectedSeatIndex < seats.GetLength(0) * seats.GetLength(1) - seats.GetLength(1))
-            {
-                selectedSeatIndex += seats.GetLength(1);
-                DrawSeats();
-            }
-        }
-
-        private void MoveSelectionLeft()
-        {
-            if (selectedSeatIndex % seats.GetLength(1) > 0)
-            {
-                selectedSeatIndex--;
-                DrawSeats();
-            }
-        }
-
-        private void MoveSelectionRight()
-        {
-            if (selectedSeatIndex % seats.GetLength(1) < seats.GetLength(1) - 1)
-            {
-                selectedSeatIndex++;
-                DrawSeats();
-            }
-        }
-
-        private void SelectSeat()
-        {
-            int row = selectedSeatIndex / seats.GetLength(1);
-            int col = selectedSeatIndex % seats.GetLength(1);
-            if (seats[row, col] == "[ ]")
-            {
-                seats[row, col] = "[X]"; // Mark the seat as taken
-                DrawSeats();
-                Console.WriteLine("Seat selected!");
-                Console.WriteLine("Want to select more? ");
-                string choice = Console.ReadLine();
-                switch (choice)
-                {
-                    case "y":
-                        DrawSeats();
-                        break;
-                    case "n":
-                        Program.Main();
-                        break;
-                }
-                DrawSeats();
-            }
-            else
-            {
-                Console.WriteLine("Seat already taken. Please select another seat.");
-                
+                case ConsoleKey.UpArrow:
+                    MoveSelectionUp();
+                    break;
+                case ConsoleKey.DownArrow:
+                    MoveSelectionDown();
+                    break;
+                case ConsoleKey.LeftArrow:
+                    MoveSelectionLeft();
+                    break;
+                case ConsoleKey.RightArrow:
+                    MoveSelectionRight();
+                    break;
+                case ConsoleKey.Enter:
+                    SelectSeat();
+                    break;
             }
         }
     }
+
+    static void InitializeSeats()
+    {
+        for (int i = 0; i < seats.GetLength(0); i++)
+        {
+            for (int j = 0; j < seats.GetLength(1); j++)
+            {
+                
+                seats[i, j] = "[ ]";
+                
+
+            }
+            
+        }
+
+        // Left upper corner (seats[y,x])
+        seats[0, 0] = "   ";
+        seats[1,0] = "   ";
+        seats[2,0] = "   ";
+        seats[0,1] = "   ";
+        
+        // Right upper corner
+        seats[0, 11] = "   ";
+        seats[1, 11] = "   ";
+        seats[2, 11] = "   ";
+        seats[0, 10] = "   ";
+        
+        // Lower left corner
+        seats[13, 0] = "   ";
+        seats[13, 1] = "   ";
+        seats[12, 0] = "   ";
+        seats[12, 1] = "   ";
+        seats[11,0] = "   ";
+        
+        // lower right corner
+        seats[13, 11] = "   ";
+        seats[13, 10] = "   ";
+        seats[12, 10] = "   ";
+        seats[12, 11] = "   ";
+        seats[11,11] = "   ";
+    }
+
+    static void DrawSeats()
+    {
+        Console.Clear();
+        Console.WriteLine("Select a seat:");
+        
+        for (int i = 0; i < seats.GetLength(0); i++)
+        {
+            for (int j = 0; j < seats.GetLength(1); j++)
+            {
+                //Base Color setter in blue
+                CinemaBaseColor();
+                
+                // Base Color setter for yellow circle 
+                // CinemaBaseColorYellow();
+
+                if (i == selectedSeatIndex / seats.GetLength(1) && j == selectedSeatIndex % seats.GetLength(1))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green; // Highlight selected seat
+                }
+                Console.Write(seats[i, j] + " ");
+                Console.ResetColor();
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void MoveSelectionUp()
+    {
+        if (selectedSeatIndex >= seats.GetLength(1))
+        {
+            selectedSeatIndex -= seats.GetLength(1);
+            DrawSeats();
+        }
+    }
+
+    static void MoveSelectionDown()
+    {
+        if (selectedSeatIndex < seats.GetLength(0) * seats.GetLength(1) - seats.GetLength(1))
+        {
+            selectedSeatIndex += seats.GetLength(1);
+            DrawSeats();
+        }
+    }
+
+    static void MoveSelectionLeft()
+    {
+        if (selectedSeatIndex % seats.GetLength(1) > 0)
+        {
+            selectedSeatIndex--;
+            DrawSeats();
+        }
+    }
+
+    static void MoveSelectionRight()
+    {
+        if (selectedSeatIndex % seats.GetLength(1) < seats.GetLength(1) - 1)
+        {
+            selectedSeatIndex++;
+            DrawSeats();
+        }
+    }
+
+    static void SelectSeat()
+    {
+        int row = selectedSeatIndex / seats.GetLength(1);
+        int col = selectedSeatIndex % seats.GetLength(1);
+        if (seats[row, col] == "[ ]")
+        {
+            seats[row, col] = "[X]"; // Mark the seat as taken
+            
+            
+            Console.WriteLine("Seat selected!");
+            DrawSeats();
+        }
+      
+        else
+        {
+            Console.WriteLine("Seat already taken. Please select another seat.");
+        }
+
+    }
+    
+    // Hier komt de indx met de verschillende kleuren
+
+    static void ColorIndex()
+    {
+
+
+    }
+    
+    // Proberen maken van de clear functie zodat Color index werkt
+
+    private static void CinemaBaseColor()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        
+    }
+    
+    
+    private static  void CinemaBaseColorYellow()
+    {
+
+
+    }
+    
+}
