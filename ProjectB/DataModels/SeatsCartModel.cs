03fd38a206dbd9b380202278468ad7fbb94e4d22
Revin400 <57983479+Revin400@users.noreply.github.com>
2023-05-31 18:35:05 +0200

    Add files via upload
    Edits SeatCarModel & SeatCar.json. Also added the seat color discription with Jullian.

---
+++ ProjectB/DataModels/SeatsCartModel.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectB.DataModels;
+using System.Text.Json.Serialization;
+public class SeatsCartModel
+{
+    [JsonPropertyName("SeatColor")]
+    public string SeatColor { get; set; }
+    [JsonPropertyName("TotalPrice")]
+    public double TotalPrice { get; set; }
+
+    public SeatsCartModel(string seatColor, double totalPrice)
+    {
+        SeatColor = seatColor;
+        TotalPrice = totalPrice;
+    }
+}
