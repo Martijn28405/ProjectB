e6a8f04cbeabb32641d11565cf24ec356af940e5
Luca Stuart <lucastuart1610@hotmail.com>
2023-08-13 17:25:38 +0200

    ideal en paypal error handeling

--- ProjectB/Logic/PaymentLogic/PaypalPaymentLogic.cs
+++ ProjectB/Logic/PaymentLogic/PaypalPaymentLogic.cs
@@ -5,8 +5,18 @@ public class PaypalPaymentLogic : IPaymentLogic
         Console.WriteLine("Welcome to the login page of PayPal!");
         Console.WriteLine("Username: ");
         string username = Console.ReadLine();
+        while (string.IsNullOrEmpty(username) || int.TryParse(username, out _))
+        {
+            Console.WriteLine("Invalid input. Please try again:");
+            username = Console.ReadLine();
+        }
         Console.WriteLine("Password: ");
         string password = Console.ReadLine();
+        while (string.IsNullOrEmpty(password))
+        {
+            Console.WriteLine("Invalid input. Please try again:");
+            password = Console.ReadLine();
+        }
         if (username != null && password != null)
         {
             Console.WriteLine($"Your login was correct, you've paid {price} succesfully.");
@@ -16,4 +26,5 @@ public class PaypalPaymentLogic : IPaymentLogic
         Console.WriteLine("Login failed");
         Payment(price);
     }
+
 }
